a9da4c5b6fa06930ffc9743d1c393b0c8a37e4ff
Kim HyunWoo <stories2@naver.com>
2021-05-22 09:09:45 +0900

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const app = new PIXI.Application({
-    width: window.innerWidth, height: window.innerHeight, transparent: true, resolution: window.devicePixelRatio || 1,
+    width: window.innerWidth, height: window.innerHeight, resizeTo: window, transparent: true, resolution: window.devicePixelRatio || 1,
 });
 console.log('window.devicePixelRatio', window.devicePixelRatio);
 document.body.appendChild(app.view);
@@ -9,16 +9,27 @@ const container = new PIXI.Container();
 app.stage.addChild(container);
 
 // Create a new texture
-const texture = PIXI.Texture.from('src/images/sprite.png');
-
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
-    const bunny = new PIXI.Sprite(texture);
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
+// const texture = PIXI.Texture.from('src/images/sprite.png');
+//
+// // Create a 5x5 grid of bunnies
+// for (let i = 0; i < 25; i++) {
+//     const bunny = new PIXI.Sprite(texture);
+//     bunny.anchor.set(0.5);
+//     bunny.x = (i % 5) * 40;
+//     bunny.y = Math.floor(i / 5) * 40;
+//     container.addChild(bunny);
+// }
+const connechan_1_sprite = PIXI.Texture.from('src/images/partition/connechan_1.png');
+for (let i = 0; i < app.screen.width; i += 50) {
+    for (let t = 0; t < app.screen.height; t += 50) {
+        const conneChan = new PIXI.Sprite(connechan_1_sprite);
+        conneChan.x = i;
+        conneChan.y = t;
+        container.addChild(conneChan);
+    }
 }
+
+
 console.log('w x h', app.screen.width, app.screen.height)
 // Move container to the center
 container.x = app.screen.width / 2;
@@ -46,5 +57,10 @@ app.stage.addChild(connechanAnimSprite);
 app.ticker.add((delta) => {
     // rotate the container!
     // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
+    // container.rotation -= 0.01 * delta;
 });
+
+// window.addEventListener('resize', () => {
+//     app.view.style.width = window.innerHeight + 'px';
+//     app.view.style.height = window.innerHeight + 'px';
+// })
