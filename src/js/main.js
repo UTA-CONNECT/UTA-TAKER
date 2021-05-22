576878ef2adf7764948b6e50517989ed037dc0f5
Kim HyunWoo <stories2@naver.com>
2021-05-23 01:48:36 +0900

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -400,8 +400,10 @@ app.ticker.add((delta) => {
     itemContainer.x = Math.sin(map.shakeX) * 8 + app.screen.width / 2;
     hudContainer.x = Math.sin(map.shakeX) * 8 + app.screen.width / 2;
 
-    if (map.shakeX != 0) {
+    if (map.shakeX > 0) {
         map.shakeX += (0 - delta);
+    } else {
+        map.shakeX = 0;
     }
 });
 
